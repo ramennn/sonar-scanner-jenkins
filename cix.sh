40eb9cfcb58f7deeafd5d1a0c69ccb80df2a1846
Tom <thomas.verin@sonarsource.com>
2016-04-14 16:10:46 +0200

    retrieve built artifact

--- cix.sh
+++ cix.sh
@@ -4,7 +4,16 @@ echo "Running with SQ=$SQ_VERSION"
 
 JENKINS_VERSION=1.580.3
  
-  mvn package -B -e -V -DskipTests
-  
-  cd its
-  mvn -Djenkins.runtimeVersion="$JENKINS_VERSION" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false verify 
+#deploy the version built by travis
+CURRENT_VERSION=`mvn help:evaluate -Dexpression="project.version" | grep -v '^\[\|Download\w\+\:'`
+RELEASE_VERSION=`echo $CURRENT_VERSION | sed "s/-.*//g"`
+NEW_VERSION="$RELEASE_VERSION-build$CI_BUILD_NUMBER"
+echo $NEW_VERSION
+mkdir -p target
+cd target
+curl --user $ARTIFACTORY_QA_READER_USERNAME:$ARTIFACTORY_QA_READER_PASSWORD -sSLO https://repox.sonarsource.com/sonarsource-public-qa/org/jenkins-ci/plugins/sonar/$NEW_VERSION/sonar-$NEW_VERSION.hpi
+mv sonar-$NEW_VERSION.hpi sonar.hpi
+cd ..
+
+cd its
+mvn -Djenkins.runtimeVersion="$JENKINS_VERSION" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false verify 
